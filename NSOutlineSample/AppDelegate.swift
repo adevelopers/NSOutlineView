6bd0422287783fddafc75ef7c7169cbe2ae2cf4b
adeveloper2015 <hudiakov@gmail.com>
2017-04-10 16:32:25 +0300

    Initial Commit

---
+++ NSOutlineSample/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NSOutlineSample
+//
+//  Created by adeveloper on 10.04.17.
+//  Copyright © 2017 adeveloper. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
